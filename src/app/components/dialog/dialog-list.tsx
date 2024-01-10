e4d5fee5146f6c72c64f2cb7befec1d0283cb938
wly <wangleyan0735@163.com>
2024-01-10 21:16:26 +0800

    feature:dialog_update

--- src/app/components/dialog/dialog-list.tsx
+++ src/app/components/dialog/dialog-list.tsx
@@ -1,87 +1,41 @@
-import {useEffect, useState} from "react";
-import {DialogType} from "@/types/chat";
-import {DialogListItem} from "./dialog-item"
-import {DialogResizeableSidebar} from "./dialog-resizeable-sidebar"
-import styles from "./dialog-head.module.scss"
+import styles from "./dialog-list.module.scss";
+import {DialogListItem} from "./dialog-list-item";
+import {DialogResizeableSidebar} from "@/app/components/dialog/dialog-resizeable-sidebar";
 import {useNavigate} from "react-router-dom";
+import {userChatStore} from "@/app/store/chat-store";
+import {DialogHead} from "@/app/components/dialog/dialog-head";
+
+/**
+ * 对话框列表
+ */
 export function DialogList() {
-    // 使用useState钩子管理对话数组
-    const [dialogs, setDialogs] = useState<DialogType[]>([]);
-    // 使用useState钩子管理当前选中的对话
-    const [selected, setSelected] = useState<DialogType>();
     const navigate = useNavigate();
+    const chatStore = userChatStore();
+    const [sessions, currentSessionIndex, selectSession] = userChatStore(
+        (state) => [
+            state.sessions,
+            state.currentSessionIndex,
+            state.selectSession]);
 
-
-    // 渲染对话列表
     return (
         // DialogResizeableSidebar 用于调整对话栏的大小
         <DialogResizeableSidebar>
-            {/* 对话列表头部，包含搜索框和创建会话按钮 */}
-            <div className={styles["dialog-head"]}>
-                <div className={styles["dialog-search-box"]}><input type="text" placeholder="搜索"/></div>
-                <div className={styles["dialog-search-add"]} onClick={() => {
-
-                    // 心里咨询
-                    const dialog01: DialogType = {
-                        avatar: '/role/bugstack.png',
-                        dialogId: Math.floor(Math.random() * 900) + 100,
-                        read: true,
-                        subTitle: '有什么可以帮你的吗？',
-                        timestamp: Date.now(),
-                        title: '直接对话',
-                        count: Math.floor(Math.random() * 90)
-                    };
-
-                    const dialog02: DialogType = {
-                        avatar: '/role/interview.png',
-                        dialogId: Math.floor(Math.random() * 900) + 100,
-                        read: true,
-                        subTitle: '请回答一下Java的基础类型有哪些？',
-                        timestamp: Date.now(),
-                        title: '面试官',
-                        count: Math.floor(Math.random() * 90)
-                    };
-
-                    const dialog03: DialogType = {
-                        avatar: '/role/psychological.png',
-                        dialogId: Math.floor(Math.random() * 900) + 100,
-                        read: true,
-                        subTitle: '吹灭别人的灯，不能照亮自己',
-                        timestamp: Date.now(),
-                        title: '心里咨询',
-                        count: Math.floor(Math.random() * 90)
-                    };
-
-                    const idx = Math.floor(Math.random() * 3) + 1;
-                    if (1 === idx) {
-                        dialogs.unshift(dialog01);
-                        setSelected(dialog01);
-                    }
-
-                    if (2 === idx) {
-                        dialogs.unshift(dialog02);
-                        setSelected(dialog02);
-                    }
-
-                    if (3 === idx) {
-                        dialogs.unshift(dialog03);
-                        setSelected(dialog03);
-                    }
-
-                }}></div>
-            </div>
+            {/*头部操作*/}
+            <DialogHead/>
             {/*对话列表*/}
             <div className={styles["dialog-list"]}>
-                {/*循环遍历数据，当有数据变更时会自动刷新到页面*/}
-                {dialogs.map((dialog, index) => (
+                {sessions.map((session, index) => (
                     <DialogListItem
-                        key={dialog.dialogId}
-                        dialog={dialog}
-                        selected={selected?.dialogId === dialog.dialogId}
+                        key={session.id}
+                        session={session}
+                        selected={currentSessionIndex === index}
                         onClick={() => {
                             // 点击时跳转到对应的界面，并传递必要参数信息
-                            navigate(`/chat/${dialog.dialogId}`, {state: {title: dialog.title}})
-                            setSelected(dialog)
+                            selectSession(index);
+                            navigate(`/chat/${session.id}`, {state: {title: session.dialog.title}})
+                        }}
+                        onClickDelete={() => {
+                            chatStore.deleteSession(index);
                         }}
                     />
                 ))}
--- src/app/components/dialog/dialog-message-item.tsx
+++ src/app/components/dialog/dialog-message-item.tsx
@@ -1,36 +1,46 @@
 import styles from './dialog-message-item.module.scss'
 import {Avatar, Space} from "antd";
-import {Message, MessageRole, MessageDirection} from "@/types/chat";
-import {RefObject} from "react";
+import {Message, MessageDirection, MessageRole} from "@/types/chat";
+import { RefObject } from 'react';
+import { Markdown } from '@/app/components/markdown/markdown';
 
-
+/**
+ * 用对象封装属性，方便扩展
+ */
 interface Props {
     message: Message;
-    //parentRef?: RefObject<HTMLDivElement>;
-
+    parentRef?: RefObject<HTMLDivElement>;
 }
+
+/**
+ * 对话面板消息元素
+ * @constructor
+ */
 export function DialogMessageItem(props: Props) {
-    const {message} = props;
-    const isReceive = message.direction === MessageDirection.Receive;
-    return (
-        <Space className={`${styles.messageWrapper} ${isReceive ? styles.receive : styles.send}`}>
-            {isReceive ? (
-                <>
+    const {message, parentRef} = props;
+    const isUser = message.role === MessageRole.user;
+    return <>
+        <div
+            className={
+                isUser ? styles["chat-message-user"] : styles["chat-message"]
+            }
+        >
+            <div className={styles["chat-message-container"]}>
+                <div className={styles["chat-message-avatar"]}>
                     <Avatar shape="square" src={message.avatar} size={40} style={{
                         borderRadius: '4px',
                         backgroundColor: '#f6f6f6'
                     }}/>
-                    <p className={styles.message}>{message.content}</p>
-                </>
-            ) : (
-                <>
-                    <p className={styles.message}>{message.content}</p>
-                    <Avatar shape="square" src={message.avatar} size={40} style={{
-                        borderRadius: '4px',
-                        backgroundColor: '#f6f6f6'
-                    }}/>
-                </>
-            )}
-        </Space>
-    );
+                </div>
+                <div className={styles["chat-message-item"]}>
+                    <Markdown
+                        content={message.content}
+                        fontSize={14}
+                        parentRef={parentRef}
+                        defaultShow={false}
+                    />
+                </div>
+            </div>
+        </div>
+    </>
 }
