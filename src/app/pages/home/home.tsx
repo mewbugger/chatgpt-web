6722c79c605f5f199d85eaf49f99605cfc052610
wly <wangleyan0735@163.com>
2024-01-10 21:17:18 +0800

    feature:home_role_update

--- src/app/pages/home/home.tsx
+++ src/app/pages/home/home.tsx
@@ -9,6 +9,7 @@ const Role = dynamic(async () => (await import("@/app/pages/role/role")).Role, {
 import {Path} from "@/app/constants";
 import { useAppConfig } from "../../store/config";
 import {DialogMessage} from "@/app/components/dialog/dialog-message";
+import {RoleDetail} from "@/app/components/role/role-detail";
 
 
 function Screen() {
@@ -25,7 +26,9 @@ function Screen() {
                     <Route path={Path.Chat} element={<Chat/>}>
                         <Route path=":id" element={<DialogMessage/>} />
                     </Route>
-                    <Route path={Path.Role} element={<Role/>}/>
+                    <Route path={Path.Role} element={<Role/>}>
+                        <Route path=":id" element={<RoleDetail/>} />
+                    </Route>
                 </Routes>
             </div>
         </div>
