ff3511aa126e79b4afbd748115ac440407c2bb47
wly <wangleyan0735@163.com>
2024-01-10 16:28:44 +0800

    feature:config

---
+++ src/types/chat.ts
@@ -0,0 +1,16 @@
+export interface DialogType {
+    // 头像
+    avatar: string;
+    // 对话ID
+    dialogId: number;
+    // 是否阅读
+    read: boolean;
+    // 小标题
+    subTitle: string;
+    // 对话最后时间
+    timestamp: number;
+    // 聊天头
+    title: string;
+    // 消息数
+    count: number;
+}
